fad263d367fc7e14861c611b0edcffad9603f4f1
387anu1213 <387aniis1213@gmail.com>
2026-08-23 19:16:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -263,7 +263,7 @@ if st.button("💰 COBRAR", use_container_width=True):
 
         # Meme del hermano de Lamine Yamal en el mundial
         # meme_lamine = "https://i.imgflip.com/92jz8x.jpg"
-        meme = https://i.kym-cdn.com/photos/images/newsfeed/002/681/313/4e9.jpg
+        meme = "https://i.kym-cdn.com/photos/images/newsfeed/002/681/313/4e9.jpg"
         st.image(meme, width=250)
 
 
